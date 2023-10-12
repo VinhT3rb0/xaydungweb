30d6dc6eb9cde783be6eb2abd0d9c8663b8ad164
VinhT3rb0 <121910355+VinhT3rb0@users.noreply.github.com>
2023-10-12 22:18:21 +0700

    code

--- product.js
+++ product.js
@@ -3,36 +3,88 @@ var valueProduct = parseFloat(document.getElementById('value-product').value);
             valueProduct=1;
         }
         function downPrt() {
-        var valueProduct = parseFloat(document.getElementById('value-product').value);
+            valueProduct = parseFloat(document.getElementById('value-product').value);
             valueProduct -=1;
             if(valueProduct<=0)
                 valueProduct=1;
             document.getElementById('value-product').value= valueProduct;
         }
         function upPrt() {
-        var valueProduct = parseFloat(document.getElementById('value-product').value);
+            valueProduct = parseFloat(document.getElementById('value-product').value);
             valueProduct +=1;
             document.getElementById('value-product').value= valueProduct;
 
         }
 
-        const buyBtn = document.querySelector('.btn-buy-product')
+        // mở đăng nhập
+        const showLogin = document.querySelector('.show-login')
         const modal = document.querySelector('.js-modal')
         const modalClose = document.querySelector('.js-modal-close')
-        const modalContainer = document.querySelector('.js-modal-container')
+        const modalLoginOut = document.querySelector('.buy-ticket-js')
+        function showLogins() {
+            modal.style = 'display: flex'
+        }
+        function hideLogins() {
+            modal.style = 'display: none'
+        }
+        showLogin.addEventListener('click',showLogins)
+        modalClose.addEventListener('click',hideLogins)
+        modalLoginOut.addEventListener('click',hideLogins)
+        modal.addEventListener('click',hideLogins)
+
+
+        // mở giỏ hàng
+        const add = document.querySelector('.btn-buy-product')
+        const showCart = document.querySelector('.show-cart')
+        const cart = document.querySelector('.cart')
+        const cartClose = document.querySelector('.icon-close')
+        const cartContainer = document.querySelector('.cart-container')
         // hàm hiển thị mua vé (thêm class open vào modal)
         function showBuyTickets() {
-            modal.classList.add('open')
+            cart.style = 'display: flex'
         }
-        // hàm ẩn modal mua vé(gỡ bỏ class open của modal)
         function hideBuyTickets() {
-            modal.classList.remove('open')
+            cart.style = 'display: none'
         }
-        // lặp qua từng thẻ button và nghe hành vi click 
-            buyBtn.addEventListener('click', showBuyTickets)
-        // nghe hành vi click vào button close 
-        modalClose.addEventListener('click',hideBuyTickets)
-        modal.addEventListener('click',hideBuyTickets)
-        modalContainer.addEventListener('click', function(event) {
+            add.addEventListener('click', showBuyTickets)
+            showCart.addEventListener('click', showBuyTickets)
+        cartClose.addEventListener('click',hideBuyTickets)
+        cartContainer.addEventListener('click', function(event) {
             event.stopPropagation()
         })
+
+        // giỏ hàng
+        const buyBtn = document.querySelector('.btn-buy-product')
+        buyBtn.addEventListener('click',function(event) {{
+            var btnItem = event.target
+            var product = btnItem.parentElement
+            var productImg = product.querySelector("img").src;
+            var productName = product.querySelector(".title-product").innerText
+            var productPrice = product.querySelector(".price").innerText
+            addcart(productImg,productName,productPrice)
+        }})
+        function addcart(productImg,productName,productPrice) {
+            var addtr = document.createElement("tr")
+             valueProduct = parseFloat(document.getElementById('value-product').value);
+            var trcontent = '<tr><td style="display: flex; flex-direction: column;align-items: center; width: 100px;"><img class="cart-img" src="'+productImg+'" alt="">'+productName+'</td><td><p><span>'+productPrice+'</span><sup>đ</sup></p></td><td><input style="width: 40px;" type="number" value="'+valueProduct+'" min="0"></td><td style="cursor: pointer;">Xóa</td></tr>'
+            addtr.innerHTML = trcontent
+            var cartTable = document.querySelector("tbody")
+            cartTable.append(addtr)
+            cartTotal()
+        }
+        // tính tổng tiền
+        function cartTotal() {
+            var cartItem = document.querySelectorAll("tbody tr")
+            var totalB = 0
+            for(var i=0;i<cartItem.length;i++) {
+                var inputValue = cartItem[i].querySelector("input").value
+                var productPrice = cartItem[i].querySelector("span").innerHTML
+                var totalPrice = inputValue * productPrice*1000
+                totalB += totalPrice
+                bill = totalB.toLocaleString('de-DE')
+            }
+            var totalBill = document.querySelector('.price-total span')
+            totalBill.innerHTML = bill
+            console.log(totalBill);
+        }
+
--- started.js
+++ started.js
@@ -6,8 +6,9 @@ window.addEventListener("load", function() {
     const sliderItems = document.querySelectorAll(".slider-item");
     const sliderItemWidth = sliderItems[0].offsetWidth;
     const slidesLength = sliderItems.length;
+    console.log(slidesLength);
     let positionX = 0;
-    let index = 0;
+    let index = 1;
     nextBtn.addEventListener("click", function() {
         handleChangeSlide(1);
     });
@@ -17,25 +18,49 @@ window.addEventListener("load", function() {
     function handleChangeSlide(direction) {
         if(direction === 1) {
             index++;
-            if(index>=slidesLength) {
+            if(index>slidesLength) {
                 index = slidesLength;
                 return;
             }
             positionX = positionX - sliderItemWidth;
-            sliderMain.style = 'transform: translateX(${positionX}px)';
+            sliderMain.style = `transform: translateX(${positionX}px)`;
             console.log(positionX);
             console.log("next slide");
         } else if (direction === -1) {
             index--;
             if(index<=0) {
-                index=0;
+                index=1;
                 return;
             }
             positionX = positionX + sliderItemWidth;
-            sliderMain.style = "transform: translateX(1366px)";
+            sliderMain.style = `transform: translateX(${positionX}px)`;
             console.log(positionX);
 
             console.log("prev slide");
         }
     }
 });
+
+
+        const login = document.querySelector('.login-js')
+        const buyBtn = document.querySelector('.login')
+        const modal = document.querySelector('.js-modal')
+        const modalClose = document.querySelector('.js-modal-close')
+        const modalContainer = document.querySelector('.js-modal-container')
+        // hàm hiển thị mua vé (thêm class open vào modal)
+        function showBuyTickets() {
+            modal.style = 'display: flex'
+        }
+        // hàm ẩn modal mua vé(gỡ bỏ class open của modal)
+        function hideBuyTickets() {
+            modal.style = 'display: none'
+        }
+        // lặp qua từng thẻ button và nghe hành vi click 
+            buyBtn.addEventListener('click', showBuyTickets)
+        // nghe hành vi click vào button close 
+        modalClose.addEventListener('click',hideBuyTickets)
+        modal.addEventListener('click',hideBuyTickets)
+        login.addEventListener('click',hideBuyTickets)
+        modalContainer.addEventListener('click', function(event) {
+            event.stopPropagation()
+        })
